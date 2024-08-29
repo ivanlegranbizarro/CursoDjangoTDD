1679b2f74edd814b4a28f0e5f788a25d35f61a3b
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-08-29 08:47:11 +0200

    test post author

--- posts/tests.py
+++ posts/tests.py
@@ -1,28 +1,43 @@
 from http import HTTPStatus
-
+from django.contrib.auth.models import User
 from django.test import TestCase
-
 from .models import Post
 
-# Create your tests here.
+
+# Base class with the general setup
+class BaseTestCase(TestCase):
+    def setUp(self):
+        # Create a user for all tests
+        self.user = User.objects.create_user(
+            username="testuser",
+            password="testpassword",
+        )
+        # Create posts for all tests
+        self.post1 = Post.objects.create(
+            title="test title",
+            content="test content",
+            author=self.user,
+        )
+        self.post2 = Post.objects.create(
+            title="test title2",
+            content="test content2",
+            author=self.user,
+        )
 
 
-class PostModelTests(TestCase):
+# Tests for Post model
+class PostModelTests(BaseTestCase):
     def test_post_model_exists(self):
         posts = Post.objects.all()
-
-        self.assertQuerySetEqual(posts, [])
+        self.assertEqual(posts.count(), 2)  # Expecting 2 posts created in setUp
 
     def test_string_representation(self):
-        post = Post.objects.create(title="test title")
-        self.assertEqual(str(post), "test title")
+        self.assertEqual(str(self.post1), "test title")
+        self.assertEqual(str(self.post2), "test title2")
 
 
-class HomePageTest(TestCase):
-    def setUp(self):
-        Post.objects.create(title="test title", content="test content")
-        Post.objects.create(title="test title2", content="test content2")
-
+# Tests for home page
+class HomePageTest(BaseTestCase):
     def test_home_page(self):
         response = self.client.get("/")
         self.assertEqual(response.status_code, HTTPStatus.OK)
@@ -37,20 +52,23 @@ class HomePageTest(TestCase):
         self.assertContains(response, "test content2")
 
 
-class DetailPageTest(TestCase):
-    def setUp(self):
-        self.post = Post.objects.create(
-            title="test title",
-            content="test content",
-        )
-        self.url = "/posts/{}/".format(self.post.id)
-
-        self.response = self.client.get(self.url)
-
+# Tests for detail page
+class DetailPageTest(BaseTestCase):
     def test_detail_page(self):
-        self.assertEqual(self.response.status_code, HTTPStatus.OK)
-        self.assertTemplateUsed(self.response, "posts/detail.html")
+        url = f"/posts/{self.post1.id}/"
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertTemplateUsed(response, "posts/detail.html")
 
     def test_detail_page_returns_post(self):
-        self.assertContains(self.response, self.post.title)
-        self.assertContains(self.response, self.post.content)
+        url = f"/posts/{self.post1.id}/"
+        response = self.client.get(url)
+        self.assertContains(response, self.post1.title)
+        self.assertContains(response, self.post1.content)
+
+
+# Tests for Post author
+class PostAuthorTest(BaseTestCase):
+    def test_post_author(self):
+        self.assertEqual(self.post1.author, self.user)
+        self.assertEqual(self.post2.author, self.user)
