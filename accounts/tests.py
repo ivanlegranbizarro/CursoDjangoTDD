56b2dbf924088fd8cb9d389a0fa464150625506b
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-08-29 13:26:12 +0200

    test signup

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,11 +1,18 @@
 from http import HTTPStatus
 
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.test import TestCase
+
+from .forms import UserRegistrationForm
 
 # Create your tests here.
 
 
 class AccountCreationTest(TestCase):
+    def setUp(self):
+        self.form_class = UserRegistrationForm
+
     def test_signup_page_exists(self):
         url = "/accounts/signup/"
         response = self.client.get(url)
@@ -13,3 +20,29 @@ class AccountCreationTest(TestCase):
         self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertTemplateUsed(response, "accounts/signup.html")
         self.assertContains(response, "Create your account")
+
+    def test_signup_form_works_correctly(self):
+        self.assertTrue(issubclass(self.form_class, UserCreationForm))
+        self.assertTrue("username" in self.form_class.base_fields)
+        self.assertTrue("email" in self.form_class.base_fields)
+        self.assertTrue("password1" in self.form_class.base_fields)
+        self.assertTrue("password2" in self.form_class.base_fields)
+
+    def test_signup_form_submission(self):
+        form = self.form_class()
+        sample_data = {
+            "username": "testuser",
+            "email": "Jt2pK@example.com",
+            "password1": "testpassword",
+            "password2": "testpassword",
+        }
+
+        form = self.form_class(sample_data)
+        self.assertTrue(form.is_valid())
+
+        form.save()
+
+        self.assertEqual(User.objects.count(), 1)
+
+        user = User.objects.get(username="testuser")
+        self.assertEqual(user.email, "Jt2pK@example.com")
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 
+from .forms import UserRegistrationForm
+
 # Create your views here.
 
 
 def signup(request):
-    return render(request, "accounts/signup.html")
+    form = UserRegistrationForm()
+    context = {"form": form}
+    return render(request, "accounts/signup.html", context)
