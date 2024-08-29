ee52e3b13a5334f28b49d29093fe9eb82abed607
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-08-29 17:54:31 +0200

    login pasado

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect, render
 
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login
 
 from .forms import UserRegistrationForm
 
@@ -23,12 +24,19 @@ def signup(request):
 
 
 def login_page(request):
+    form = AuthenticationForm()
     if request.method == "POST":
         form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+
             return redirect("posts:home")
-    else:
-        form = AuthenticationForm()
 
     context = {"form": form}
 
